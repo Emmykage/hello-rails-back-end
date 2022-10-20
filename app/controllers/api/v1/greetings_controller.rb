a96f27a5f138c7bfe34c16a243f4fd65586f5274
Menanya <emmiemenz@gmail.com>
2022-10-20 12:53:15 +0100

    refracture Api class

--- app/controllers/api/v1/greetings_controller.rb
+++ app/controllers/api/v1/greetings_controller.rb
@@ -1,7 +1,13 @@
-class Api::V1::GreetingsController < ApplicationController
-    def index
+# frozen_string_literal: true
+
+module Api
+  module V1
+    class GreetingsController < ApplicationController
+      def index
         random_num = rand(Greeting.count)
         @greeting = Greeting.all[random_num].message
         render json: @greeting
+      end
     end
+  end
 end
